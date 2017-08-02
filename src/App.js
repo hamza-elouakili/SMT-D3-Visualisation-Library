6c6ac6e7e22a6b9eee0d637a24a43f6d103ae02a
Hamza El Ouakili <hamzaelouakili@Hamzas-MacBook-Pro.local>
2017-08-02 16:54:49 +0200

    final

--- src/App.js
+++ src/App.js
@@ -5,18 +5,25 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = { percentComplete: 0.3 }
-    this.togglePercent = this.togglePercent.bind(this)
+    this.randomPercent = this.randomPercent.bind(this)
   }
 
-  togglePercent() {
-    const percentage = this.state.percentComplete === 0.3 ? 0.7 : 0.3
+  componentDidMount() {
+    const percentage = Math.random()
     this.setState({ percentComplete: percentage })
   }
+
+  randomPercent() {
+    const percentage = Math.random()
+    this.setState({ percentComplete: percentage })
+  }
+
   render() {
     console.log(this.state.percentComplete)
+    const style = {}
     return (
       <div>
-        <a onClick={this.togglePercent}>Toggle Arc</a>
+        <a onClick={this.randomPercent}>Random percentage</a>
         <ProgressArc
           height={300}
           width={300}
